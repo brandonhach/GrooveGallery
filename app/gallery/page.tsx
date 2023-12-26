6e7fa67d706c2a0f860c2e42a41874cc7a910387
brandonhach <specialcambo@gmail.com>
2023-12-26 12:42:58 -0500

    Added component MasonGallery

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import MasonGallery from '../components/MasonGallery/MasonGallery';
 
-const page = () => {
-	return <div>page</div>;
+const gallery = () => {
+	return (
+		<>
+			<div className='w-screen h-screen flex justify-center items-center'>
+				<div className='w-5/6 h-full'>
+					<MasonGallery></MasonGallery>
+				</div>
+			</div>
+		</>
+	);
 };
 
-export default page;
+export default gallery;
