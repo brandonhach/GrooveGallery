3c827d872abff92a05fecc5bfa5ce9d1cf3274c3
brandonhach <specialcambo@gmail.com>
2024-01-04 12:08:57 -0500

    This method query client and returns client data

--- api/repositories/client_repository.py
+++ api/repositories/client_repository.py
@@ -16,6 +16,19 @@ class ClientRepository:
         client = self.db_session.query(Client).filter(Client.username == username).first()
         return client.password_hash
     
+    def get_client_to_json(self, username):
+        client = self.db_session.query(Client).filter(Client.username == username).first()
+        
+        if client:
+            client_data = {
+                "username": client.username,
+                "email": client.email,
+                "date_created": client.date_created
+            }
+            return client_data
+        else:
+            return {"message": "Client is not found. Bad username."}, 401
+    
 
 
 
