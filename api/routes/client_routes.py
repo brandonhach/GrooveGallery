36876ed563e3d95aa0dcdc9c1153b2aed056dcd6
brandonhach <specialcambo@gmail.com>
2024-01-04 12:10:07 -0500

    Login and register route will return a json that contains both client data and token info

--- api/routes/client_routes.py
+++ api/routes/client_routes.py
@@ -8,8 +8,8 @@ def create_client_blueprint(client_service):
         data = request.get_json()
         try:
             client_service.create_user(data['username'], data['email'], data['password'])
-            access_token = client_service.authenticate_user(data['username'], data['password'])
-            return jsonify({"access_token": access_token, "message": "Registration successful"}), 200
+            session_info = client_service.authenticate_user(data['username'], data['password'])
+            return jsonify({"session_info": session_info, "message": "Registration successful"}), 200
         except Exception as e:
             return jsonify({"error": str(e)}), 400
     
@@ -17,9 +17,9 @@ def create_client_blueprint(client_service):
     def login():
         data = request.get_json()
         try:
-            access_token = client_service.authenticate_user(data['username'], data['password'])
-            if access_token:
-                return jsonify({"access_token": access_token, "message": "Login successful"}), 200
+            session_info = client_service.authenticate_user(data['username'], data['password'])
+            if session_info:
+                return jsonify({"session_info": session_info, "message": "Login successful"}), 200
             else:
                 return jsonify({"message": "Bad username or password"}), 401
         except Exception as e:
